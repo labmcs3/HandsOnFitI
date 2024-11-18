74d57512c6b9c0296461ae0bc5d4803d63fef55c
Fabrizio Parodi <fabrizio.parodi@ge.infn.it>
2024-11-18 17:39:21 +0100

    update

--- fitlin.py
+++ fitlin.py
@@ -1,18 +1,18 @@
 from   iminuit import Minuit
-from   numpy   import *
+import numpy as np
 import matplotlib.pyplot as plt
 
-def f(x,b,a):
-    return a*x+b
+def f(x,p):
+    return p[1]*x+p[0]
 
-def fcn(a,b):
+def fcn(par):
     val = 0
     for i in range(0,len(x)):
-        val = val + ((y[i]-f(x[i],a,b))/ey[i])**2
+        val = val + ((y[i]-f(x[i],par))/ey[i])**2
     return val
 
 # Acquisizione dati
-x,y,ex,ey = loadtxt('pendolo.dat',usecols=(0,1,2,3),unpack=True)
+x,y,ex,ey = np.loadtxt('pendolo.dat',usecols=(0,1,2,3),unpack=True)
 
 ## Chiamare Minuit 
 ## Stampare la matrice di correlazione (correlation)
